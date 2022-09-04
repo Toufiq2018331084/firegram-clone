71024902a9063f325b59404f703614c19e7c436b
Toufiq2018331084 <52928065+Toufiq2018331084@users.noreply.github.com>
2022-09-05 04:15:26 +0600

    config file error fixed

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase/app";
-import 'firebase/storage';
-import 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getStorage} from 'firebase/storage';
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,7 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const projectStorage = firebase.storage();
-const projectFirestore = firebase.fireststore();
+const projectStorage = getStorage();
+const projectFirestore = getFirestore();
 
 export {projectFirestore, projectStorage};
